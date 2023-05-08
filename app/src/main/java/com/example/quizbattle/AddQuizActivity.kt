bff1199cf0ed73f03834d83a73fa1c4e1da36154
zaiom333 <zaiom333@gmail.com>
2023-05-09 00:35:49 +0200

    ESSA6

--- app/src/main/java/com/example/quizbattle/AddQuizActivity.kt
+++ app/src/main/java/com/example/quizbattle/AddQuizActivity.kt
@@ -39,7 +39,7 @@ class AddQuizActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener 
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
         val item: String = p0?.getItemAtPosition(p2).toString()
-        Toast.makeText(p0?.context, item, Toast.LENGTH_SHORT).show()
+        //Toast.makeText(p0?.context, item, Toast.LENGTH_SHORT).show()
         Constants.node = item
 
 
